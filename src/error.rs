01aab84c983b60fcfd79c36245ebe455d145fb34
sokorototo <nyachiengatoto@gmail.com>
2022-04-29 16:46:51 +0300

    Moved error handling into it's own file

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,9 @@
 pub struct ErrorCode;
 
+extern "C" {
+	pub fn exit(exit_code: u8, offset: usize) -> !;
+}
+
 #[allow(non_upper_case_globals)]
 impl ErrorCode {
 	pub(crate) const UnableToWriteToIPC: u8 = 1;
--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,17 @@
-#![no_main]
-
 use std::io::Read;
 
-mod error;
-mod repetition;
-mod task;
 mod tests;
 
-#[no_mangle]
-#[cfg(debug_assertions)]
-pub unsafe extern "C" fn main() {
-	log("Hello World!");
-}
+mod console;
+mod error;
 
-extern "C" {
-	fn console_log(offset: usize);
-	fn exit(exit_code: u8, offset: usize) -> !;
-}
+mod goal;
+mod preprocessor;
+mod schedule;
+mod task;
+
+#[no_mangle]
+extern "C" fn entry() {}
 
 #[no_mangle]
 pub unsafe extern "C" fn get_data_pointer() -> *const u8 {
@@ -26,22 +21,6 @@ pub unsafe extern "C" fn get_data_pointer() -> *const u8 {
 #[no_mangle]
 pub unsafe extern "C" fn get_ipc_buffer_size() -> usize {
 	IPC_BUFFER_SIZE
-}
-
-/// Log data to the console
-pub fn log<S: AsRef<str>>(msg: S) {
-	let data = msg.as_ref().as_bytes();
-
-	unsafe {
-		if data.len() >= IPC_BUFFER_SIZE {
-			let error_msg: &[u8] = b"The length of data to be logged to the console exceeds the size of the IPC_BUFFER";
-			write_to_ipc(error_msg);
-			exit(error::ErrorCode::LogDataTooLong, error_msg.len())
-		};
-
-		write_to_ipc(data);
-		console_log(data.len());
-	}
 }
 
 /// A buffer where IPC Data is written to by Rust and read from by Javascript
@@ -55,6 +34,6 @@ pub(crate) unsafe fn write_to_ipc<R: Read>(mut source: R) {
 		let data = error.to_string();
 		IPC_BUFFER[..data.len()].copy_from_slice(data.as_bytes());
 
-		exit(error::ErrorCode::UnableToWriteToIPC, data.len());
+		error::exit(error::ErrorCode::UnableToWriteToIPC, data.len());
 	}
 }
