0ed2f6de750fc6287b2229b5641d13a9831ebe90
sokorototo <nyachiengatoto@gmail.com>
2022-04-29 04:08:51 +0300

    just testing logging

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@ use std::io::Read;
 mod tests;
 
 /// A buffer where IPC Data is written to by Rust and read from by Javascript
-/// At any one moment, only one read and write is done to this buffer, so it's wrapped in a Mutex
-pub const IPC_BUFFER_SIZE: usize = 1024;
-pub static mut IPC_BUFFER: [u8; 1024] = [0; IPC_BUFFER_SIZE];
+/// At any one moment, only one read and write is done to this buffer, WASM is single-threaded anyway
+pub const IPC_BUFFER_SIZE: usize = 1024 * 64;
+pub static mut IPC_BUFFER: [u8; IPC_BUFFER_SIZE] = [0; IPC_BUFFER_SIZE];
 
 /// This writes some data to the IPC buffer, then returns a pointer and an offset to the data
 pub(crate) fn write_to_ipc<R: Read>(mut source: R) -> Result<usize, String> {
@@ -29,8 +29,8 @@ extern "C" {
 	fn console_log(offset: usize);
 }
 
-pub fn log<S: AsRef<str>>(data: S) -> Result<(), String> {
-	let data = data.as_ref().as_bytes();
+pub fn log<S: AsRef<str>>(msg: S) -> Result<(), String> {
+	let data = msg.as_ref().as_bytes();
 
 	if data.len() >= IPC_BUFFER_SIZE {
 		return Err("The length of data to be logged to the console exceeds the size of the IPC_BUFFER".into());
@@ -46,9 +46,11 @@ pub fn log<S: AsRef<str>>(data: S) -> Result<(), String> {
 
 #[no_mangle]
 pub unsafe extern "C" fn main() -> i8 {
-	if let Err(_) = log("Hello from the other siiiiiiiiide") {
-		return -1;
-	};
+	for _ in 0..12 {
+		if log("A log called at will from Rust").is_err() {
+			return -1;
+		};
+	}
 
 	0
 }
