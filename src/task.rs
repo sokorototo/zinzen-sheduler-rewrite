bbf63bfbc4f41faa364b66ebd09a75ef0388992f
sokorototo <nyachiengatoto@gmail.com>
2022-04-30 18:19:53 +0300

    A simple struct for serializing Tasks

--- src/task.rs
+++ src/task.rs
@@ -17,3 +17,37 @@ impl<'a> Task<'a> {
 		Task { goal, completed: false }
 	}
 }
+
+/// A simple wrapper around an slice of tasks for easy serialization
+pub(crate) struct Tasks<'a>(&'a [Task<'a>]);
+
+impl<'a> Tasks<'a> {
+	pub fn serialize_json(&self) -> String {
+		let mut json = String::new();
+
+		self.0.into_iter().for_each(|task| {
+			if json.is_empty() {
+				json.push_str("[");
+			} else {
+				json.push_str(",");
+			}
+
+			let serialized_task = task.serialize_json();
+			json.push_str(&serialized_task);
+		});
+
+		if json.is_empty() {
+			json.push_str(&"[]");
+		} else {
+			json.push(']');
+		}
+
+		json
+	}
+}
+
+impl<'a> From<&'a [Task<'a>]> for Tasks<'a> {
+	fn from(tasks: &'a [Task<'a>]) -> Self {
+		Self(tasks)
+	}
+}
